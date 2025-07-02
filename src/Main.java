2e9e3f40fb57f855fe9730448853881a062b451d
FiggyFoxFunc <131496393+jack-adorbs@users.noreply.github.com>
2025-07-02 19:12:28 +0100

    Basic frame created on run.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import gui.BaseFrame;
+
+/**
+ * Main entry point of Mnemosyne.
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BaseFrame frame = new BaseFrame();
+
+        // Display.
+        frame.setVisible(true);
     }
 }
