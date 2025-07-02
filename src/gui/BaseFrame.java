47c789aad4137a0b32773f562456521b32cb1651
FiggyFoxFunc <131496393+jack-adorbs@users.noreply.github.com>
2025-07-02 23:04:33 +0100

    Added to BaseFrame and MainMenu.

--- src/gui/BaseFrame.java
+++ src/gui/BaseFrame.java
@@ -4,9 +4,30 @@ import javax.swing.*;
 import java.awt.*;
 
 public class BaseFrame extends JFrame {
+    GridBagConstraints gridBagConstraints;
+    MainMenu mainMenu;
+
     public BaseFrame() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Mnemosyne");
         setMinimumSize(new Dimension(800, 600));
+
+        // Layout setup.
+        prepareLayout(2, 2);
+        prepareMainMenu();
+    }
+
+    public void prepareLayout(Integer width, Integer height) {
+        setLayout(new GridBagLayout());
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridwidth = width;
+        gridBagConstraints.gridheight = height;
+    }
+
+    public void prepareMainMenu() {
+        mainMenu = new MainMenu();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
+        add(mainMenu, gridBagConstraints);
     }
 }
--- src/gui/MainMenu.java
+++ src/gui/MainMenu.java
@@ -1,7 +1,14 @@
 package gui;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * MainMenu represents the initial Menu shown upon running Mnemosyne.
  */
-public class MainMenu {
+public class MainMenu extends JPanel {
+    public MainMenu() {
+        JLabel test = new JLabel("Test");
+        add(test);
+    }
 }
