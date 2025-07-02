40891749bcb72808ef455fc202638f6e44fed30d
FiggyFoxFunc <131496393+jack-adorbs@users.noreply.github.com>
2025-07-02 23:03:41 +0100

    Added default constructor.

--- src/zettelkasten/Zettelkasten.java
+++ src/zettelkasten/Zettelkasten.java
@@ -1,8 +1,22 @@
 package zettelkasten;
 
+import java.util.ArrayList;
+
 /**
  * Zettelkasten represents the PKM system as a whole.
  *
  */
 public class Zettelkasten {
+    private final ArrayList<Card> cards;
+
+    /**
+     * Default Constructor for Zettelkasten.
+     */
+    public Zettelkasten() {
+        cards = new ArrayList<>();
+    }
+
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
 }
